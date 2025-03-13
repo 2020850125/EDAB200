0c7cfb1b46b363f0578ec34f72985000eec8f9aa
2020850125 <2020850125@ufs4life.ac.za>
2025-03-13 08:38:14 +0200

    Update App.py

--- App.py
+++ App.py
@@ -1,9 +1,27 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import streamlet as st
 
+
+st..title("this is my streamlit app fpr 13 march")
+st.heading ("this modolu code EDAB6808")
+ST.subheading("Thisnisna fun and interactive module")
+
+            
 # Generate random time series data
 time_series = np.random.randn(100)
+
+variable = st.button ("this is my button")
+if variable
+  time_series = np.random.randn(2000)
+  fig,ax = plt.subplots()
+  ax.plot(tine_series)
+  ax.set_title("this is my graph")
+  ax.set_xlabel("units")
+  ax.set_ylabel("value")
+  st.pyplot(fig)
+
 
 # Plot the time series
 plt.plot(time_series)
